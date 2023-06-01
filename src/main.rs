9af49c4e84354a2fe73c43b11bd37fc67b79782c
Serp <manu.serpette74@gmail.com>
2023-05-31 23:50:46 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{Window, WindowType};
+
 fn main() {
-    println!("Hello, world!");
+    gtk::init().expect("Failed to initialize GTK.");
+
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("Hello, World!");
+    window.set_default_size(350, 70);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window.show_all();
+    gtk::main();
 }
